ea2c5fe38c131255fa35fd08086203b5c4ed949c
Philip Hallstrom <philip@pjkh.com>
2011-07-22 11:13:40 -0700

    test new argument passing

---
+++ test/arguments_test.rb
@@ -0,0 +1,69 @@
+require File.expand_path(File.join(File.dirname(__FILE__), 'spec_helper'))
+
+describe 'ProgressBar arguments' do
+  before do
+    @default_max = 100
+    @default_meters = [:bar, :counter, :percentage, :elapsed, :eta, :rate]
+    @default_write_every = 1
+  end
+
+  it "should set appropriate defaults without any arguments" do
+    bar = ProgressBar.new
+    bar.max.should == @default_max
+    bar.meters.should == @default_meters
+    bar.options[:write_every].should == @default_write_every
+  end
+
+  it "should allow a single argument specifying the max" do
+    bar = ProgressBar.new(123)
+    bar.max.should == 123
+    bar.meters.should == @default_meters
+    bar.options[:write_every].should == @default_write_every
+  end
+
+  it "should allow specifying just the meters" do
+    bar = ProgressBar.new(:bar, :eta)
+    bar.max.should == @default_max
+    bar.meters.should == [:bar, :eta]
+    bar.options[:write_every].should == @default_write_every
+  end
+
+  it "should allow specifying just the options" do
+    bar = ProgressBar.new(:write_every => 123)
+    bar.max.should == @default_max
+    bar.meters.should == @default_meters
+    bar.options[:write_every].should == 123
+  end
+
+  it "should allow specyfing the max and meters" do
+    bar = ProgressBar.new(123, :bar, :eta)
+    bar.max.should == 123
+    bar.meters.should == [:bar, :eta]
+    bar.options[:write_every].should == @default_write_every
+  end
+
+  it "should allow specyfing the max and options" do
+    bar = ProgressBar.new(123, :write_every => 456)
+    bar.max.should == 123
+    bar.meters.should == @default_meters
+    bar.options[:write_every].should == 456
+  end
+
+  it "should allow specyfing the meters and options" do
+    bar = ProgressBar.new(:bar, :eta, :write_every => 123)
+    bar.max.should == @default_max
+    bar.meters.should == [:bar, :eta]
+    bar.options[:write_every].should == 123
+  end
+
+  it "should allow specyfing the max, meters, and options" do
+    bar = ProgressBar.new(123, :bar, :eta, :write_every => 456)
+    bar.max.should == 123
+    bar.meters.should == [:bar, :eta]
+    bar.options[:write_every].should == 456
+  end
+
+
+
+end
+
