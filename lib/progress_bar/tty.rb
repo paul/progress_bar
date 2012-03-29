e8a76d566e128963d21bd04969405fa704caa5d1
Mathieu Martin <webmat@gmail.com>
2012-03-29 17:24:22 -0400

    Move the TTY implementation of ProgressBar to ProgressBar::TTY

---
+++ lib/progress_bar/tty.rb
@@ -0,0 +1,34 @@
+module ProgressBar
+  class TTY < Base
+    def initialize(*args)
+      require 'highline'
+      super *args
+      @hl = HighLine.new
+    end
+
+    def write
+      clear!
+      print to_s
+    end
+
+    def print(str)
+      $stderr.write str
+    end
+
+    def clear!
+      print "\r"
+    end
+
+    def terminal_width
+      # HighLine check takes a long time, so only update width every second.
+      if @last_width_adjustment.nil? || Time.now - @last_width_adjustment > 1
+        @last_width_adjustment = Time.now
+        @terminal_width = @hl.output_cols.to_i
+      else
+        @terminal_width
+      end
+    end
+
+  end
+end
+
